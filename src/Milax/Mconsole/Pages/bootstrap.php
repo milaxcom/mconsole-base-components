5241d76438424283b68de8923527eb55cb04a147
Eugene Min <e.v.min@icloud.com>
2016-04-05 19:46:00 +1000

    New menu structure

--- src/Milax/Mconsole/Pages/bootstrap.php
+++ src/Milax/Mconsole/Pages/bootstrap.php
@@ -4,7 +4,46 @@ return [
     'name' => 'Pages',
     'identifier' => 'mconsole-pages',
     'description' => 'Creates main content for website.',
-    'menu' => [],
+    'menu' => [
+        'content' => [
+            'child' => [
+                'pages_all' => [
+                    'name' => 'All pages',
+                    'translation' => 'menu.pages.list.name',
+                    'url' => 'pages',
+                    'description' => 'menu.pages.list.description',
+                    'route' => 'mconsole.pages.index',
+                    'visible' => true,
+                    'enabled' => true,
+                ],
+                'pages_form' => [
+                    'name' => 'Create page',
+                    'translation' => 'menu.pages.form.name',
+                    'url' => 'pages/create',
+                    'description' => 'menu.pages.form.description',
+                    'route' => 'mconsole.pages.store',
+                    'visible' => true,
+                    'enabled' => true,
+                ],
+                'pages_update' => [
+                    'name' => 'Edit pages',
+                    'translation' => 'menu.pages.update.name',
+                    'description' => 'menu.pages.update.description',
+                    'route' => 'mconsole.pages.edit',
+                    'visible' => false,
+                    'enabled' => true,
+                ],
+                'pages_delete' => [
+                    'name' => 'Delete pages',
+                    'translation' => 'menu.pages.delete.name',
+                    'description' => 'menu.pages.delete.description',
+                    'route' => 'mconsole.pages.destroy',
+                    'visible' => false,
+                    'enabled' => true,
+                ],
+            ],
+        ],
+    ],
     'register' => [
         'middleware' => [],
         'providers' => [],
