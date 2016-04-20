1bbd6ba5e289f0bbdabd68c606b4005d9c0e9250
Eugene Min <e.min@milax.com>
2016-04-21 07:23:19 +1100

    Push menu with API

--- src/Milax/Mconsole/News/bootstrap.php
+++ src/Milax/Mconsole/News/bootstrap.php
@@ -7,46 +7,6 @@ return [
     'name' => 'News',
     'identifier' => 'mconsole-news',
     'description' => 'mconsole::news.module.description',
-    'menu' => [
-        'content' => [
-            'child' => [
-                'news_all' => [
-                    'name' => 'All news',
-                    'translation' => 'news.menu.list.name',
-                    'url' => 'news',
-                    'description' => 'news.menu.list.description',
-                    'route' => 'mconsole.news.index',
-                    'visible' => true,
-                    'enabled' => true,
-                ],
-                'news_form' => [
-                    'name' => 'Create news',
-                    'translation' => 'news.menu.create.name',
-                    'url' => 'news/create',
-                    'description' => 'news.menu.create.description',
-                    'route' => 'mconsole.news.create',
-                    'visible' => false,
-                    'enabled' => true,
-                ],
-                'news_update' => [
-                    'name' => 'Edit news',
-                    'translation' => 'news.menu.update.name',
-                    'description' => 'news.menu.update.description',
-                    'route' => 'mconsole.news.edit',
-                    'visible' => false,
-                    'enabled' => true,
-                ],
-                'news_delete' => [
-                    'name' => 'Delete news',
-                    'translation' => 'news.menu.delete.name',
-                    'description' => 'news.menu.delete.description',
-                    'route' => 'mconsole.news.destroy',
-                    'visible' => false,
-                    'enabled' => true,
-                ],
-            ],
-        ],
-    ],
     'register' => [
         'middleware' => [],
         'providers' => [],
@@ -61,6 +21,41 @@ return [
         Installer::uninstall();
     },
     'init' => function () {
+        app('API')->menu->push('content', 'news_all', [
+            'name' => 'All news',
+            'translation' => 'news.menu.list.name',
+            'url' => 'news',
+            'description' => 'news.menu.list.description',
+            'route' => 'mconsole.news.index',
+            'visible' => true,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'news_form', [
+            'name' => 'Create news',
+            'translation' => 'news.menu.create.name',
+            'url' => 'news/create',
+            'description' => 'news.menu.create.description',
+            'route' => 'mconsole.news.create',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'news_update', [
+            'name' => 'Edit news',
+            'translation' => 'news.menu.update.name',
+            'description' => 'news.menu.update.description',
+            'route' => 'mconsole.news.edit',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'news_delete', [
+            'name' => 'Delete news',
+            'translation' => 'news.menu.delete.name',
+            'description' => 'news.menu.delete.description',
+            'route' => 'mconsole.news.destroy',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        
         // Register in search engine
         app('API')->search->register(function ($text) {
             return News::select('id', 'slug', 'heading')->where('slug', 'like', sprintf('%%%s%%', $text))->orWhere('title', 'like', sprintf('%%%s%%', $text))->orWhere('heading', 'like', sprintf('%%%s%%', $text))->orWhere('preview', 'like', sprintf('%%%s%%', $text))->orWhere('text', 'like', sprintf('%%%s%%', $text))->get()->transform(function ($page) {
--- src/Milax/Mconsole/Pages/bootstrap.php
+++ src/Milax/Mconsole/Pages/bootstrap.php
@@ -7,46 +7,6 @@ return [
     'name' => 'Pages',
     'identifier' => 'mconsole-pages',
     'description' => 'mconsole::pages.module.description',
-    'menu' => [
-        'content' => [
-            'child' => [
-                'pages_all' => [
-                    'name' => 'All pages',
-                    'translation' => 'pages.menu.list.name',
-                    'url' => 'pages',
-                    'description' => 'pages.menu.list.description',
-                    'route' => 'mconsole.pages.index',
-                    'visible' => true,
-                    'enabled' => true,
-                ],
-                'pages_form' => [
-                    'name' => 'Create page',
-                    'translation' => 'pages.menu.create.name',
-                    'url' => 'pages/create',
-                    'description' => 'pages.menu.create.description',
-                    'route' => 'mconsole.pages.create',
-                    'visible' => false,
-                    'enabled' => true,
-                ],
-                'pages_update' => [
-                    'name' => 'Edit pages',
-                    'translation' => 'pages.menu.update.name',
-                    'description' => 'pages.menu.update.description',
-                    'route' => 'mconsole.pages.edit',
-                    'visible' => false,
-                    'enabled' => true,
-                ],
-                'pages_delete' => [
-                    'name' => 'Delete pages',
-                    'translation' => 'pages.menu.delete.name',
-                    'description' => 'pages.menu.delete.description',
-                    'route' => 'mconsole.pages.destroy',
-                    'visible' => false,
-                    'enabled' => true,
-                ],
-            ],
-        ],
-    ],
     'register' => [
         'middleware' => [],
         'providers' => [],
@@ -61,6 +21,41 @@ return [
         Installer::uninstall();
     },
     'init' => function () {
+        app('API')->menu->push('content', 'pages_all', [
+            'name' => 'All pages',
+            'translation' => 'pages.menu.list.name',
+            'url' => 'pages',
+            'description' => 'pages.menu.list.description',
+            'route' => 'mconsole.pages.index',
+            'visible' => true,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'pages_form', [
+            'name' => 'Create page',
+            'translation' => 'pages.menu.create.name',
+            'url' => 'pages/create',
+            'description' => 'pages.menu.create.description',
+            'route' => 'mconsole.pages.create',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'pages_update', [
+            'name' => 'Edit pages',
+            'translation' => 'pages.menu.update.name',
+            'description' => 'pages.menu.update.description',
+            'route' => 'mconsole.pages.edit',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'pages_delete', [
+            'name' => 'Delete pages',
+            'translation' => 'pages.menu.delete.name',
+            'description' => 'pages.menu.delete.description',
+            'route' => 'mconsole.pages.destroy',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        
         // Register in search engine
         app('API')->search->register(function ($text) {
             return Page::select('id', 'slug', 'heading')->where('slug', 'like', sprintf('%%%s%%', $text))->orWhere('title', 'like', sprintf('%%%s%%', $text))->orWhere('heading', 'like', sprintf('%%%s%%', $text))->orWhere('preview', 'like', sprintf('%%%s%%', $text))->orWhere('text', 'like', sprintf('%%%s%%', $text))->get()->transform(function ($page) {
