b7ebdcf158999a8f268003edeb626746d7abd710
Eugene Min <e.min@milax.com>
2016-04-23 17:08:54 +1100

    English translation

---
+++ src/Milax/Mconsole/News/assets/resources/lang/en/news.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'module' => [
+        'description' => 'Manage news',
+    ],
+    'menu' => [
+        'list' => [
+            'name' => 'News',
+            'description' => 'List news',
+        ],
+        'create' => [
+            'name' => 'Add news',
+            'description' => 'Add an entry in news feed',
+        ],
+        'update' => [
+            'name' => 'Edit news',
+            'description' => 'Update information in added news',
+        ],
+        'delete' => [
+            'name' => 'Delete news',
+            'description' => 'Delete from news feed',
+        ],
+    ],
+    'table' => [
+        'published' => 'Published',
+        'updated' => 'Updated',
+        'slug' => 'Address',
+        'heading' => 'Title',
+    ],
+    'form' => [
+        'indexing' => 'Search engines indexing',
+        'date' => 'Date',
+        'heading' => 'Title',
+        'slug' => 'Slug',
+        'preview' => 'Preview text',
+        'text' => 'Full text',
+        'seo' => 'SEO Settings',
+        'title' => 'Title',
+        'description' => 'Description',
+        'gallery' => 'Gallery',
+        'cover' => 'Cover',
+    ],
+    'settings' => [
+        'index' => 'Number of news at index page',
+        'archive' => 'Number of news in archive',
+        'gallery' => 'Use galleries',
+        'cover' => 'Use cover',
+        'group' => [
+            'name' => 'News',
+        ],
+    ],
+];
---
+++ src/Milax/Mconsole/Pages/assets/resources/lang/en/pages.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'module' => [
+        'description' => 'Pages',
+    ],
+    'menu' => [
+        'list' => [
+            'name' => 'Pages',
+            'description' => 'Show page list',
+        ],
+        'create' => [
+            'name' => 'Add page',
+            'description' => 'New pages of the site, with additional links and images',
+        ],
+        'update' => [
+            'name' => 'Edit page',
+            'description' => 'the Update of information, additional links and attached images',
+        ],
+        'delete' => [
+            'name' => 'Delete page',
+            'description' => 'Delete information with additional links and attached images',
+        ],
+    ],
+    'table' => [
+        'updated' => 'Updated',
+        'slug' => 'Slug',
+        'heading' => 'Heading',
+    ],
+    'form' => [
+        'content' => 'Content',
+        'gallery' => 'Gallery',
+        'cover' => 'Cover',
+        'options' => 'Settings',
+        'slug' => 'Slug',
+        'heading' => 'Heading',
+        'preview' => 'Preview text',
+        'text' => 'Full text',
+        'seo' => 'SEO Settings',
+        'title' => 'Title',
+        'description' => 'Description',
+        'indexing' => 'Search engines indexing',
+        'hide_heading' => 'Hide header',
+        'fullwidth' => 'Full width',
+        'enabled' => 'Published',
+        'links' => [
+            'label' => 'Links',
+            'title' => 'Text',
+            'url' => 'URL',
+            'enabled' => 'Enabled',
+        ],
+    ],
+];
