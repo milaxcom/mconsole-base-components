01012ffcabf263c1f0fbb4f394c0411965c2d65f
Eugene Min <e.min@milax.com>
2016-04-25 22:57:23 +1100

    #8
    Added migration for new pages column

---
+++ src/Milax/Mconsole/Pages/assets/migrations/2016_04_25_115352_modify_pages_table_add_links_set_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyPagesTableAddLinksSetColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->integer('links_page_id')->default(0)->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropColumn('links_page_id');
+        });
+    }
+}
