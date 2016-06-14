453239bdea292866d410b0c1863d79eac22b64a9
vladimir-r <rudenko@milax.ru>
2016-06-14 17:51:22 +1100

    Add method findBySlug for page model

--- src/Milax/Mconsole/Pages/Contracts/Repositories/PagesRepository.php
+++ src/Milax/Mconsole/Pages/Contracts/Repositories/PagesRepository.php
@@ -4,4 +4,12 @@ namespace Milax\Mconsole\Pages\Contracts\Repositories;
 
 interface PagesRepository
 {
+    /**
+     * Get page by slug
+     * 
+     * @param  string $slug
+     * @param  string $lang [Language key]
+     * @return Milax\Mconsole\Pages|Models\Page
+     */
+    public function findBySlug($slug, $lang = null);
 }
--- src/Milax/Mconsole/Pages/Repositories/PagesRepository.php
+++ src/Milax/Mconsole/Pages/Repositories/PagesRepository.php
@@ -4,8 +4,20 @@ namespace Milax\Mconsole\Pages\Repositories;
 
 use Milax\Mconsole\Repositories\EloquentRepository;
 use Milax\Mconsole\Pages\Contracts\Repositories\PagesRepository as Repository;
+use Milax\Mconsole\Contracts\ContentLocalizator;
 
 class PagesRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\Pages\Models\Page::class;
+    
+    public function __construct(ContentLocalizator $localizator)
+    {
+        $this->localizator = $localizator;
+    }
+    
+    public function findBySlug($slug, $lang = null)
+    {
+        $page = $this->query()->where('slug', $slug)->firstOrFail();
+        return $this->localizator->localize($page, $lang);
+    }
 }
