b8cb871303001dc3b27f2d761576eea1f3beca23
Eugene Min <e.min@milax.com>
2016-06-18 16:16:35 +1100

    Use new ContentCompiler contract

--- src/Milax/Mconsole/Pages/Repositories/PagesRepository.php
+++ src/Milax/Mconsole/Pages/Repositories/PagesRepository.php
@@ -4,20 +4,20 @@ namespace Milax\Mconsole\Pages\Repositories;
 
 use Milax\Mconsole\Repositories\EloquentRepository;
 use Milax\Mconsole\Pages\Contracts\Repositories\PagesRepository as Repository;
-use Milax\Mconsole\Contracts\ContentLocalizator;
+use Milax\Mconsole\Contracts\ContentCompiler;
 
 class PagesRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\Pages\Models\Page::class;
     
-    public function __construct(ContentLocalizator $localizator)
+    public function __construct(ContentCompiler $compiler)
     {
-        $this->localizator = $localizator;
+        $this->compiler = $compiler;
     }
     
     public function findBySlug($slug, $lang = null)
     {
         $page = $this->query()->where('slug', $slug)->firstOrFail();
-        return $this->localizator->localize($page, $lang);
+        return $this->compiler->set($page)->localize($lang)->render()->get();
     }
 }
