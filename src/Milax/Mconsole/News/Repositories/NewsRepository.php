4bba471582fa7255ccaf80175a6093bf94e11dae
vladimir-r <rudenko@milax.ru>
2016-07-25 19:26:48 +1100

    Add localize compilter

--- src/Milax/Mconsole/News/Repositories/NewsRepository.php
+++ src/Milax/Mconsole/News/Repositories/NewsRepository.php
@@ -4,8 +4,31 @@ namespace Milax\Mconsole\News\Repositories;
 
 use Milax\Mconsole\Repositories\EloquentRepository;
 use Milax\Mconsole\News\Contracts\Repositories\NewsRepository as Repository;
+use Milax\Mconsole\Contracts\ContentCompiler;
 
 class NewsRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\News\Models\News::class;
+    
+    public function __construct(ContentCompiler $compiler)
+    {
+        $this->compiler = $compiler;
+    }
+    
+    public function getCompiled()
+    {
+        $news = $this->query()->enabled()->get();
+        
+        foreach ($news as $key => $new) {
+            $news[$key] = $this->findBySlug($new->slug, \App::getLocale());
+        }
+        
+        return $news;
+    }
+    
+    public function findBySlug($slug, $lang = null)
+    {
+        $page = $this->query()->where('slug', $slug)->firstOrFail();
+        return $this->compiler->set($page)->localize($lang)->render()->get();
+    }
 }
